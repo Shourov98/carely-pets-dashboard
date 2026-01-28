5009888303bc0d35fa78e5eb25a299c648becba8
Shourov <mdmahbub0412@gmail.com>
2026-01-28 06:31:43 +0600

    [fix]: next config

--- next.config.ts
+++ next.config.ts
@@ -11,6 +11,10 @@ const nextConfig: NextConfig = {
         protocol: "https",
         hostname: "plus.unsplash.com",
       },
+      {
+        protocol: "https",
+        hostname: "carelypet-storage.s3.eu-north-1.amazonaws.com",
+      },
     ],
   },
 };
